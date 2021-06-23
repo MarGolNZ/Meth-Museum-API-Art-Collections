2e3b0467c63ebeb354c22540898e2ab0fbd87385
marina <golovina.marina@gmail.com>
2021-06-23 10:19:57 +1000

    Add external API

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
 
-import { getWelcome } from '../api'
+import { getObjectIds } from '../api'
 
-function App () {
-  const [welcomeStatement, setWelcomeStatement] = useState('')
+function App() {
+  const [objectIds, setObjectIds] = useState([])
 
   useEffect(() => {
-    getWelcome()
-      .then(res => {
-        setWelcomeStatement(res.statement)
+
+    getObjectIds()
+      .then(result => {
+        setObjectIds(result.objectIDs[0])
         return null
       })
       .catch((err) => {
@@ -17,7 +18,10 @@ function App () {
   })
 
   return (
-    <h1>{welcomeStatement}</h1>
+    <ul>
+      <li>{objectIds}</li>
+      {/* {objectIds.map(objectId => <li>{objectId}</li>)} */}
+    </ul>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const request = require('superagent')
 
 const welcome = require('./routes/welcome')
 
@@ -9,5 +10,14 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/api/v1/welcome', welcome)
+
+// this is for CORS
+// server.get('/public/collection/v1/objects', (req, res) => {
+//     console.log(req.body)
+//     request
+//         .get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
+//         .then(response => res.json(response.body))
+//         .catch(e => console.log(e))
+// })
 
 module.exports = server
